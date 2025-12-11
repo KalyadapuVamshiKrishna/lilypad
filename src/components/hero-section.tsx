a90129502bd0d2af5e5e191e9be07f2cefb9adfe
Kalyadapu Vamshi Krishna <curiuskrish@gmail.com>
2025-12-11 17:21:51 +0530

    fixed minor bugs

--- src/components/hero-section.tsx
+++ src/components/hero-section.tsx
@@ -173,7 +173,7 @@ export function HeroSection() {
           <div className="relative z-20 flex gap-10 lg:-ml-110 xl:-ml-20 phone-wrapper-fix items-center">
 
             {/* PHONE 1 */}
-            <div className="relative w-[220px] sm:w-[300px] lg:w-[300px] xl:w-[340px] shrink-0 transition-transform duration-700 ease-out">
+            <div className="relative w-[220px] sm:w-[300px] lg:w-[300px] xl:w-[340px] shrink-0 transition-transform duration-700 ease-out sm:m-15 md:mt-5 ">
               <Image
                 src="/images/hero-dashboard.png"
                 alt="Dashboard"
@@ -202,7 +202,7 @@ export function HeroSection() {
                      - Make number LARGER (e.g., -left-[150px]) -> Moves LEFT (further out)
                      - Make number SMALLER (e.g., -left-[50px]) -> Moves RIGHT (closer to phone)
               */}
-              <div className="absolute top-[30%] -left-[60px] sm:-left-[100px] lg:-left-[55px] floating-card-fix z-30 animate-bounce-slow origin-top-right scale-80 sm:scale-90 lg:scale-100">
+              <div className="absolute top-[30%] -left-[60px] sm:-left-[100px] lg:-left-[55px] floating-card-fix z-30 animate-bounce-slow origin-top-right scale-80 sm:scale-90 lg:scale-100 sm:m-5">
                 <GemininCard />
               </div>
             </div>
@@ -214,7 +214,7 @@ export function HeroSection() {
                 alt="App Interface"
                 width={800}
                 height={1600}
-                className="w-full h-auto rounded-[4.5rem] shadow-2xl"
+                className="w-full h-auto rounded-[4.5rem] sm:rounded-[1rem] shadow-2xl"
               />
 
               <div className="absolute bottom-6 left-1/2 w-[85%] -translate-x-1/2 drop-shadow-md z-20">
